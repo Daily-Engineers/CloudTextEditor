08a38ad2a1d7e373fa27928940bee3e166cba6d3
Adam Collins <adamcollinssecure@gmail.com>
2018-02-05 12:48:26 -0800

    Added database schema models and config template file

--- routes/pages.js
+++ routes/pages.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
 
-router.get('/pages/:page_id',(req,res)=>{
-  res.send({page:req.params});
+//Finds all pages where owner is :username
+router.get('/pages/:username', (req,res)=>{
+  mongoose.model('pages').find({'owner':req.params.username},(err,pages)=>{
+    res.send(pages);
+  })
 });
+
+//Finds all pages
+router.get('/pages', (req,res)=>{
+  mongoose.model('pages').find((err,pages)=>{
+    res.send(pages);
+  })
+})
 
 module.exports = router;
