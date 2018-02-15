8dcf29749dc774acbc733e111917a8a9ec3eae06
Ali Meysam Mohebbi <alimeysam@gmail.com>
2018-02-15 13:16:04 -0800

    reverted to fix merge in wrong direction

--- app.js
+++ app.js
@@ -10,34 +10,27 @@ const uri = config.getDatabaseURI();
 //connect to database
 mongoose.connect(uri);
 //Loads all mongoose models
-fs.readdirSync(__dirname + '/models').forEach((filename) => {
-  require(__dirname + '/models/' + filename)
+fs.readdirSync(__dirname + '/models').forEach((filename)=>{
+  require(__dirname+'/models/'+filename)
 })
 
 // Parsers
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
+app.use(bodyParser.urlencoded({ extended: false}));
 
 //setup handlbars view engine
-app.engine('handlebars', hbs({
-  defaultLayout: 'main'
-}));
+app.engine('handlebars', hbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 //Define routes
 app.use(require('./routes/pages'));
 app.use(require('./routes/users'));
 
-// Serve static files
-app.use('/public', express.static('public'));
 
-//Serve landing page
-app.get('/', (req, res) => res.render('editor'));
+app.get('/',(req,res)=>res.render('home'));
 
 
 //listen on port 3000
-app.listen(3000, () => {
+app.listen(3000, ()=>{
   console.log("Listening on port 3000!");
 })
