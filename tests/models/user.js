0ae4dc145313194760d1b69eaec018e9d3994da5
Brandon <funtime.spik330@gmail.com>
2018-03-09 19:01:17 -0800

    User model testing

--- tests/models/user.js
+++ tests/models/user.js
@@ -1,46 +1,37 @@
 const chai = require('chai')
 const chaiHttp = require('chai-http');
 const app = require('../../app.js');
-const http = require('http');
 const User = require('../../models/users');
-
 
 const assert = chai.assert;
 const expect = chai.expect;
 
+
 chai.use(chaiHttp);
 const agent = chai.request.agent(app)
-
-describe('/register', function() {
-    it('Should request to create a user in the database', function(){
-        chai.request(app).post('/register').send({username: 'TestCase', password: 'password'}).end(function(err, res){
-            expect(res).to.have.status(200);
-            done();
-        })
-    })
-});
-
-
-
-//TODO fix this. 'it' assert is called before the data base returns resalts
-describe('dB.user',function(){
-    it('Should see to see if the user exists in the database',async function(){
-        var user = await User.findOne({'username': 'testcase'}).exec(function(err, user){
-            if(user){
-                console.log("eefef")
-                done()
-            }
-        })
-    })
-});
-
-//also doesnt check and just asserts
-describe('/login', function() {
-    it('Should request to create a user in the database', function(){
-        agent.post('/login').send({username: 'TestCase', password: 'password'}).then(function(res){
-            return agent.get('/login').then(function(res){
-
+    describe('/register', function () {
+        it('Should request to create a user in the database', function () {
+            chai.request(app).post('/register').send({
+                username: 'TestCase',
+                password: 'password'
+            }).end(function (err, res) {
+                expect(res).to.have.status(200);
+                done();
             })
         })
+    });
+
+
+    describe('dB.user', function () {
+        it('Should see to see if the user exists in the database', async function () {
+            var user = await User.findOne({username: 'testcase'})
+            assert(user, "User does not exist");
+        })
+    });
+
+    describe('delete.user', function () {
+        it('Should delete the users', async function () {
+            var user = await User.remove({username: 'testcase'})
+            assert(user.n > 0, "User still exist or no user found");
+        })
     })
-});
